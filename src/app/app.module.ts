a3baad95410f1b93217873b77eb6c05ffa177283
Bakir Bukvic <baxoonb980@gmail.com>
2024-02-01 13:00:35 +0100

    Add basic header elements for further use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { LowerHeaderComponent } from './header/lower-header/lower-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    LowerHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/lower-header/lower-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lower-header',
+  templateUrl: './lower-header.component.html',
+  styleUrls: ['./lower-header.component.css']
+})
+export class LowerHeaderComponent {
+
+}
