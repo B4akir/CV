e32d8b31e79bfd596b33fcf4f24ad6c500a56467
Bakir Bukvic <baxoonb980@gmail.com>
2024-02-02 11:16:51 +0100

    Fix header grid system. Add buttons to linkedin, and add copy phone number and email to clipboard functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ClipboardModule } from 'ngx-clipboard';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { LowerHeaderComponent } from './header/lower-header/lower-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,12 @@ import { LowerHeaderComponent } from './header/lower-header/lower-header.compone
     LowerHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClipboardModule,
+    BrowserAnimationsModule,
+    MatTooltipModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private clipboardService: ClipboardService) {}
+
+
+  email: string="bakirbukvic2@gmail.com";
+  phoneNumber="0603067074";
+  linkedinLink: string="https://www.linkedin.com/in/bakirbukvic/";
+  linkedinImage: string= "https://store-images.s-microsoft.com/image/apps.31120.9007199266245564.44dc7699-748d-4c34-ba5e-d04eb48f7960.bc4172bd-63f0-455a-9acd-5457f44e4473";
+  phoneNumberImage: string= "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/640px-WhatsApp.svg.png";
+  emailImage: string= "https://i.pinimg.com/originals/f6/c4/f8/f6c4f8516b117c58b2eaf3f276fdfff9.png";
+
+  copyNumber(){
+    this.clipboardService.copyFromContent(this.phoneNumber);
+  }
+  copyEmail(){
+    this.clipboardService.copyFromContent(this.email);
+  }
+
 
 }
