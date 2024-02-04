331afef6e1b5ea4f9192e41724881208ddb39916
Bakir Bukvic <baxoonb980@gmail.com>
2024-02-04 10:17:09 +0100

    Add home and projects comoponents. Add routing to them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClipboardModule } from 'ngx-clipboard';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 import { HeaderComponent } from './header/header.component';
 import { LowerHeaderComponent } from './header/lower-header/lower-header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { PagesComponent } from './pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
+
+
+const  routes: Routes = [
+
+  {path:'', component: HomeComponent},
+  {path:'projects', component: ProjectsComponent},
+
+
+];
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
-    LowerHeaderComponent
+    LowerHeaderComponent,
+    PagesComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     ClipboardModule,
     BrowserAnimationsModule,
-    MatTooltipModule
+    MatTooltipModule,
+    RouterModule.forRoot(routes)
 
 
   ],
