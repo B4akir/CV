b53e07d1512594687d85d97e4f71b71fb804f22a
Bakir Bukvic <baxoonb980@gmail.com>
2024-02-08 10:31:52 +0100

    Add images to project component. Make scrolling images in projects component

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+
+ index: number=0;
+ selectedIndex: number=null;
+
+  currentImage: string="";
+
+
+//1
+
+  ngShopImgs: string[] = [
+    'assets/AngularStranica.png',
+    'assets/AngularStranica2.png',
+    'assets/AngularStranica3.png',
+    'assets/AngularStranica4.png',
+  ];
+
+  rougeImgs: string[] = [
+    'assets/Cigrica.png',
+    'assets/Cigrica2.png',
+    'assets/Cigrica3.png',
+  ];
+
+  images: string[][] = [
+    this.ngShopImgs,
+    this.rougeImgs
+  ];
+
+  isDisabled: boolean = true;
+
+
+//ngShop, rougeImgs
+
+
+
+
+
+
+
+  imgClick(selectedIndex: number){
+
+    this.selectedIndex=selectedIndex;
+    this.index=0;
+    this.isDisabled=!this.isDisabled
+    this.currentImage=this.images[this.selectedIndex][this.index];
+
+
+
+  }
+
+
+  rightNav(){
+    if (this.index < this.images[this.selectedIndex].length - 1){
+      this.index = this.index + 1;
+      this.currentImage = this.images[this.selectedIndex][this.index];
+      console.log(this.index);
+    }
+  }
+
+  leftNav(){
+
+    if (this.index!=0){
+      this.index=this.index-1;
+      this.currentImage=this.images[this.selectedIndex][this.index];
+      console.log(this.index);
+
+
+    }
+
+
+
+  }
+
+  exitImage(){
+    this.isDisabled=true;
+
+  }
+
 
 }
